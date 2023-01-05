b4fee5934dfda1d177b7a5c780edb97b5270e667
MOLIAE5 <sherice8921@gmail.com>
2023-01-05 10:47:48 -0500

    price fixing

--- pages/index.js
+++ pages/index.js
@@ -265,7 +265,7 @@ useEffect(() => {
 
                     <div className="flex items-center space-x-3">
                     <p>
-                         {Number.parseFloat((isAirdroping? 0 : isWLMint? config.wlcost : totalMinted > 7700 ? config.thirdCost : totalMinted > 4000 ? config.secondCost : isPublicSale && totalMinted > 0 ? config.firstCost : 0)*mintAmount).toFixed(
+                         {Number.parseFloat((isAirdroping? 0 : isWLMint? config.wlcost : totalMinted > 7700 ? config.thirdCost : totalMinted > 4000 ? config.secondCost :  config.firstCost )*mintAmount).toFixed(
                           4
                         )} {' '} 
                        
