734c20bbc6e2e09866e69cace1a8409efb9b1ef6
MOLIAE5 <sherice8921@gmail.com>
2023-01-06 11:34:26 -0500

    signatuire adding

--- pages/index.js
+++ pages/index.js
@@ -346,8 +346,12 @@ useEffect(() => {
       </div>
       </div>
 
-      <div className="w-screen h-auto flex flex-col items-start">
+      <div className="w-screen h-auto flex md:flex-row flex-col items-start md:items-center justify-between">
           <img src = '/logo.png' className="w-[100px] h-[135px] ml-12 mb-4"/>
+          <div className="flex flex-row justify-between items-end">
+          <img src = '/Nichel Anderson Signature 2.png' className="w-[300px] h-[100px] ml-12 mb-4"/>
+          <img src = '/Nichel Anderson .png' className="w-[100px] h-[135px] mr-12"/>    
+          </div>
         </div>
 
       </div>
